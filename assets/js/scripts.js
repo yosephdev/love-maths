2fd8354042338289e81d9c4d9f1cf325362d0a25
yosephdev <>
2024-01-05 14:15:18 +0000

    feat: Display addition question to the user

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,17 +7,24 @@ document.addEventListener('DOMContentLoaded', function(){
                 alert('You clicked submit');
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`); 
+                runGame(gameType);
             }
         })
-
-
     }
+    runGame('addition');
 })
 
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1,num2);
+
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknwon game type: ${gameType}.Aborting!`;
+    }
     
 }
 
@@ -37,8 +44,10 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
-    
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';    
 }
 
 function displaySubtractQuestion() {
